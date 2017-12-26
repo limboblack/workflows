a22f793da185c16e09a8489fd295ca6c06658390
limboblack <begumcnsln2@gmail.com>
2017-12-26 11:45:18 +0300

    added watch task to default task

--- gulpfile.js
+++ gulpfile.js
@@ -42,8 +42,10 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('builds/development/css'))
 })
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+
 
 gulp.task('watch', function () {
   gulp.watch('coffeeSources', ['coffee']);
 })
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
