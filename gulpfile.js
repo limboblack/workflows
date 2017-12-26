efad261fe262da1bfc47c6bd182e7b92dc2fcb0f
limboblack <begumcnsln2@gmail.com>
2017-12-26 15:11:47 +0300

    added gulp minify html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html');
     concat = require('gulp-concat');
 
 var env = process.env.NODE_ENV || 'development';
@@ -67,7 +68,7 @@ gulp.task('compass', function() {
 gulp.task('watch', function () {
   gulp.watch('coffeeSources', ['coffee']);
   gulp.watch('jsSources', ['js']);
-  gulp.watch('htmlSources', ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
 })
 
 gulp.task('connect', function () {
@@ -78,7 +79,9 @@ gulp.task('connect', function () {
 })
 
 gulp.task('html', function () {
-  gulp.src('htmlSources')
+  gulp.src('builds/development/*.html')
+  .pipe(gulpif(env==='production', minifyHTML()))
+  .pipe(gulpif(env==='production', gulp.dest(outputDir)))
   .pipe(connect.reload())
 })
 
